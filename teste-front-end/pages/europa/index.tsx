22580b034eb15999583d0615b96cd66bc80b65b8
G4BR-13-L <gabriel.victorc13@gmail.com>
2022-10-01 20:02:23 -0300

    Imagens dos continentes e link para a página da europa

---
+++ teste-front-end/pages/europa/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../../components/header/Header'
+
+export default function index() {
+  return (
+    <>
+    <Header/>
+
+    
+    <main>
+        <h1>Europa</h1>
+    </main>
+    </>
+  )
+}
--- teste-front-end/pages/home-sections/choose-continent-section/chooseContinentSection.tsx
+++ teste-front-end/pages/home-sections/choose-continent-section/chooseContinentSection.tsx
@@ -4,7 +4,18 @@ import styles from './chooseContinentSection.module.scss'
 import '@coreui/coreui/dist/css/coreui.min.css'
 import Image from 'next/image'
 import Europa from '../../../assets/img/Europa.png'
+import Asia from '../../../assets/img/Asia.png'
+import AmericaNorte from '../../../assets/img/America do norte.png'
+import AmericaSul from '../../../assets/img/America do sul.png'
+import Africa from '../../../assets/img/Africa.png'
+
+
+
+
+
+
 import { CCarousel, CCarouselItem, CCarouselCaption } from '@coreui/react'
+import Link from 'next/link'
 export default function ChooseContinentSection() {
     return (
         <Section>
@@ -19,37 +30,44 @@ export default function ChooseContinentSection() {
                 <div className={styles.carouselContainer}>
                     <CCarousel controls indicators>
                         <CCarouselItem>
-                            <Image className="w-300" src={Europa} alt="slide 1" />
+                                
+                            <Link href='../../europa/'>
+                                    <Image className="w-300" src={Europa} alt="slide 1" />
+                            </Link>
+                                    <CCarouselCaption className="d-none d-md-block">
+                                        <h5>Europa</h5>
+                                        <p>O continente mais antigo</p>
+                                    </CCarouselCaption>
+                                
+                        </CCarouselItem>
+
+
+                        <CCarouselItem>
+                            <Image className="w-300" src={Asia} alt="slide 2" />
                             <CCarouselCaption className="d-none d-md-block">
-                                <h5>Europa</h5>
-                                <p>O continente mais antigo</p>
+                                <h5>Asia</h5>
                             </CCarouselCaption>
                         </CCarouselItem>
 
 
                         <CCarouselItem>
-                            <Image className="w-300" src={Europa} alt="slide 2" />
+                            <Image className="w-300" src={AmericaNorte} alt="slide 3" />
                             <CCarouselCaption className="d-none d-md-block">
-                                <h5>First slide label</h5>
-                                <p>Some representative placeholder content for the first slide.</p>
+                                <h5>America do Norte</h5>
                             </CCarouselCaption>
                         </CCarouselItem>
 
 
                         <CCarouselItem>
-                            <Image className="w-300" src={Europa} alt="slide 3" />
+                            <Image className="w-300" src={Africa} alt="slide 4" />
                             <CCarouselCaption className="d-none d-md-block">
-                                <h5>First slide label</h5>
-                                <p>Some representative placeholder content for the first slide.</p>
+                                <h5>África</h5>
                             </CCarouselCaption>
                         </CCarouselItem>
-
-
                         <CCarouselItem>
-                            <Image className="w-300" src={Europa} alt="slide 4" />
+                            <Image className="w-300" src={AmericaSul} alt="slide 4" />
                             <CCarouselCaption className="d-none d-md-block">
-                                <h5>First slide label</h5>
-                                <p>Some representative placeholder content for the first slide.</p>
+                                <h5>América do Sul</h5>
                             </CCarouselCaption>
                         </CCarouselItem>
 
