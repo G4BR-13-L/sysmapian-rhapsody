d449105ef7508e94b68a255229f7adbf524fad4b
G4BR-13-L <gabriel.victorc13@gmail.com>
2022-10-01 22:03:45 -0300

    Link de retorno para a página inicial pelo header

--- teste-front-end/components/header/Header.tsx
+++ teste-front-end/components/header/Header.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import styles from './Header.module.scss'
 import logo from '../../assets/img/logo.png'
 import Image from 'next/image'
+import Link from 'next/link'
 export default function Header() {
     return (
         <div className={styles.headerContainer}>
-            <div className={styles.logoContainer}>
-                <Image src={logo} className={styles.image}/>
-            </div>
+            <Link href="/">
+                <div className={styles.logoContainer}>
+                    <Image src={logo} className={styles.image} />
+                </div>
+            </Link>
         </div>
     )
 }
